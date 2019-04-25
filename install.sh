e1ac6e819b4a7962d81450d1fb13566c48a31398
阿爪 <ljredracess@gmail.com>
2019-04-25 08:13:41 +0800

    Update and rename install.ps1 to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# git config
 ln -s ./.gitconfig ~/.gitconfig
 ln -s ./.gitignore_global ~/.gitconfig_global
+
+# tmux config
 ln -s ./.tmux.conf ~/.tmux.conf
+
+# neovim config
 mkdir -p ~/.config/nvim
 ln -s ./init.vim ~/.config/nvim/init.vim
