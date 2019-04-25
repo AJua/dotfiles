8ba74eb7e52bdf6341e7df04e8403908cd7838ec
阿爪 <ljredracess@gmail.com>
2019-04-25 08:10:16 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,5 @@
-cp ./init.vim ~/_vimrc
+ln -s ./.gitconfig ~/.gitconfig
+ln -s ./.gitignore_global ~/.gitconfig_global
+ln -s ./.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/nvim
+ln -s ./init.vim ~/.config/nvim/init.vim
